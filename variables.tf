38c2a9afe13c605e9acffc408252046cd882b79b
Dave Lipowsky <github@lipowsky.net>
2019-12-13 22:16:46 -0600

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "awsAccessKey"         { default = "xxxxx" }
+variable "awsSecretKey"         { default = "xxxxx" }
+variable "awsVpcName"           { default = "lipowsky-tf-vpc" }
+variable "awsSshKeyName"        { default = "lipowsky-aws" }
+variable "awsRegion"            { default = "us-east-2" }
+variable "awsAmiId"             { default = "ami-0917a22a0995b3f87" }
+variable "awsInstanceType"      { default = "m5.2xlarge" }
