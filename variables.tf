1cfb6598ac499f52eabf6718e3f7f04ed8da6593
Dave Lipowsky <github@lipowsky.net>
2019-12-14 19:52:48 -0600

    Added variable for name prefix

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "awsAccessKey"         { default = "xxxxx" }
 variable "awsSecretKey"         { default = "xxxxx" }
 variable "awsVpcName"           { default = "lipowsky-tf-vpc" }
+variable "awsNamePrefix"        { default = "lipowsky-tf"}
 variable "awsSshKeyName"        { default = "lipowsky-aws" }
 variable "awsRegion"            { default = "us-east-2" }
 variable "awsAmiId"             { default = "ami-0917a22a0995b3f87" }
